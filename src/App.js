151c83bbbe2d95421f63654d227460fc8941644b
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2023-03-13 22:56:57 +0300

    feat: implement tasks icon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-
+import listSvg from './assets/img/list.svg'
 
 function App() {
-  return <div className="todo">
-    <div className="todo__sidebar">
-      <button>Hello</button>
-    </div>
-    <div className="todo__tasks">
+  return (
+    <div className="todo" >
+      <div className="todo__sidebar">
+        <ul className="todo__list">
+          <li>
+            <i>
+              <img src={listSvg} alt='List icon' />
+            </i>
+            <span>Tasks</span>
+          </li>
+        </ul>
+      </div>
+      <div className="todo__tasks">
+
+      </div>
 
     </div>
-  </div>
+  );
 }
 
 export default App;
